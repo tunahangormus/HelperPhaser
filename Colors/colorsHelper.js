cbe426e1bb278cfb71bac3e00480ceaa34605a30
Tunahan Gormus <tunahangormus@gmail.com>
2019-11-13 10:54:04 +0300

    ColorsHelper Update

--- Colors/colorsHelper.js
+++ Colors/colorsHelper.js
@@ -1,302 +1,4 @@
-/**
- * @type {object}
- * @author Yusuke Kawasaki
- */
-const colors = {
-    "red": {
-        "50": "#ffebee",
-        "100": "#ffcdd2",
-        "200": "#ef9a9a",
-        "300": "#e57373",
-        "400": "#ef5350",
-        "500": "#f44336",
-        "600": "#e53935",
-        "700": "#d32f2f",
-        "800": "#c62828",
-        "900": "#b71c1c",
-        "a100": "#ff8a80",
-        "a200": "#ff5252",
-        "a400": "#ff1744",
-        "a700": "#d50000"
-    },
-    "pink": {
-        "50": "#fce4ec",
-        "100": "#f8bbd0",
-        "200": "#f48fb1",
-        "300": "#f06292",
-        "400": "#ec407a",
-        "500": "#e91e63",
-        "600": "#d81b60",
-        "700": "#c2185b",
-        "800": "#ad1457",
-        "900": "#880e4f",
-        "a100": "#ff80ab",
-        "a200": "#ff4081",
-        "a400": "#f50057",
-        "a700": "#c51162"
-    },
-    "purple": {
-        "50": "#f3e5f5",
-        "100": "#e1bee7",
-        "200": "#ce93d8",
-        "300": "#ba68c8",
-        "400": "#ab47bc",
-        "500": "#9c27b0",
-        "600": "#8e24aa",
-        "700": "#7b1fa2",
-        "800": "#6a1b9a",
-        "900": "#4a148c",
-        "a100": "#ea80fc",
-        "a200": "#e040fb",
-        "a400": "#d500f9",
-        "a700": "#aa00ff"
-    },
-    "deeppurple": {
-        "50": "#ede7f6",
-        "100": "#d1c4e9",
-        "200": "#b39ddb",
-        "300": "#9575cd",
-        "400": "#7e57c2",
-        "500": "#673ab7",
-        "600": "#5e35b1",
-        "700": "#512da8",
-        "800": "#4527a0",
-        "900": "#311b92",
-        "a100": "#b388ff",
-        "a200": "#7c4dff",
-        "a400": "#651fff",
-        "a700": "#6200ea"
-    },
-    "indigo": {
-        "50": "#e8eaf6",
-        "100": "#c5cae9",
-        "200": "#9fa8da",
-        "300": "#7986cb",
-        "400": "#5c6bc0",
-        "500": "#3f51b5",
-        "600": "#3949ab",
-        "700": "#303f9f",
-        "800": "#283593",
-        "900": "#1a237e",
-        "a100": "#8c9eff",
-        "a200": "#536dfe",
-        "a400": "#3d5afe",
-        "a700": "#304ffe"
-    },
-    "blue": {
-        "50": "#e3f2fd",
-        "100": "#bbdefb",
-        "200": "#90caf9",
-        "300": "#64b5f6",
-        "400": "#42a5f5",
-        "500": "#2196f3",
-        "600": "#1e88e5",
-        "700": "#1976d2",
-        "800": "#1565c0",
-        "900": "#0d47a1",
-        "a100": "#82b1ff",
-        "a200": "#448aff",
-        "a400": "#2979ff",
-        "a700": "#2962ff"
-    },
-    "lightblue": {
-        "50": "#e1f5fe",
-        "100": "#b3e5fc",
-        "200": "#81d4fa",
-        "300": "#4fc3f7",
-        "400": "#29b6f6",
-        "500": "#03a9f4",
-        "600": "#039be5",
-        "700": "#0288d1",
-        "800": "#0277bd",
-        "900": "#01579b",
-        "a100": "#80d8ff",
-        "a200": "#40c4ff",
-        "a400": "#00b0ff",
-        "a700": "#0091ea"
-    },
-    "cyan": {
-        "50": "#e0f7fa",
-        "100": "#b2ebf2",
-        "200": "#80deea",
-        "300": "#4dd0e1",
-        "400": "#26c6da",
-        "500": "#00bcd4",
-        "600": "#00acc1",
-        "700": "#0097a7",
-        "800": "#00838f",
-        "900": "#006064",
-        "a100": "#84ffff",
-        "a200": "#18ffff",
-        "a400": "#00e5ff",
-        "a700": "#00b8d4"
-    },
-    "teal": {
-        "50": "#e0f2f1",
-        "100": "#b2dfdb",
-        "200": "#80cbc4",
-        "300": "#4db6ac",
-        "400": "#26a69a",
-        "500": "#009688",
-        "600": "#00897b",
-        "700": "#00796b",
-        "800": "#00695c",
-        "900": "#004d40",
-        "a100": "#a7ffeb",
-        "a200": "#64ffda",
-        "a400": "#1de9b6",
-        "a700": "#00bfa5"
-    },
-    "green": {
-        "50": "#e8f5e9",
-        "100": "#c8e6c9",
-        "200": "#a5d6a7",
-        "300": "#81c784",
-        "400": "#66bb6a",
-        "500": "#4caf50",
-        "600": "#43a047",
-        "700": "#388e3c",
-        "800": "#2e7d32",
-        "900": "#1b5e20",
-        "a100": "#b9f6ca",
-        "a200": "#69f0ae",
-        "a400": "#00e676",
-        "a700": "#00c853"
-    },
-    "lightgreen": {
-        "50": "#f1f8e9",
-        "100": "#dcedc8",
-        "200": "#c5e1a5",
-        "300": "#aed581",
-        "400": "#9ccc65",
-        "500": "#8bc34a",
-        "600": "#7cb342",
-        "700": "#689f38",
-        "800": "#558b2f",
-        "900": "#33691e",
-        "a100": "#ccff90",
-        "a200": "#b2ff59",
-        "a400": "#76ff03",
-        "a700": "#64dd17"
-    },
-    "lime": {
-        "50": "#f9fbe7",
-        "100": "#f0f4c3",
-        "200": "#e6ee9c",
-        "300": "#dce775",
-        "400": "#d4e157",
-        "500": "#cddc39",
-        "600": "#c0ca33",
-        "700": "#afb42b",
-        "800": "#9e9d24",
-        "900": "#827717",
-        "a100": "#f4ff81",
-        "a200": "#eeff41",
-        "a400": "#c6ff00",
-        "a700": "#aeea00"
-    },
-    "yellow": {
-        "50": "#fffde7",
-        "100": "#fff9c4",
-        "200": "#fff59d",
-        "300": "#fff176",
-        "400": "#ffee58",
-        "500": "#ffeb3b",
-        "600": "#fdd835",
-        "700": "#fbc02d",
-        "800": "#f9a825",
-        "900": "#f57f17",
-        "a100": "#ffff8d",
-        "a200": "#ffff00",
-        "a400": "#ffea00",
-        "a700": "#ffd600"
-    },
-    "amber": {
-        "50": "#fff8e1",
-        "100": "#ffecb3",
-        "200": "#ffe082",
-        "300": "#ffd54f",
-        "400": "#ffca28",
-        "500": "#ffc107",
-        "600": "#ffb300",
-        "700": "#ffa000",
-        "800": "#ff8f00",
-        "900": "#ff6f00",
-        "a100": "#ffe57f",
-        "a200": "#ffd740",
-        "a400": "#ffc400",
-        "a700": "#ffab00"
-    },
-    "orange": {
-        "50": "#fff3e0",
-        "100": "#ffe0b2",
-        "200": "#ffcc80",
-        "300": "#ffb74d",
-        "400": "#ffa726",
-        "500": "#ff9800",
-        "600": "#fb8c00",
-        "700": "#f57c00",
-        "800": "#ef6c00",
-        "900": "#e65100",
-        "a100": "#ffd180",
-        "a200": "#ffab40",
-        "a400": "#ff9100",
-        "a700": "#ff6d00"
-    },
-    "deeporange": {
-        "50": "#fbe9e7",
-        "100": "#ffccbc",
-        "200": "#ffab91",
-        "300": "#ff8a65",
-        "400": "#ff7043",
-        "500": "#ff5722",
-        "600": "#f4511e",
-        "700": "#e64a19",
-        "800": "#d84315",
-        "900": "#bf360c",
-        "a100": "#ff9e80",
-        "a200": "#ff6e40",
-        "a400": "#ff3d00",
-        "a700": "#dd2c00"
-    },
-    "brown": {
-        "50": "#efebe9",
-        "100": "#d7ccc8",
-        "200": "#bcaaa4",
-        "300": "#a1887f",
-        "400": "#8d6e63",
-        "500": "#795548",
-        "600": "#6d4c41",
-        "700": "#5d4037",
-        "800": "#4e342e",
-        "900": "#3e2723"
-    },
-    "grey": {
-        "50": "#fafafa",
-        "100": "#f5f5f5",
-        "200": "#eeeeee",
-        "300": "#e0e0e0",
-        "400": "#bdbdbd",
-        "500": "#9e9e9e",
-        "600": "#757575",
-        "700": "#616161",
-        "800": "#424242",
-        "900": "#212121"
-    },
-    "bluegrey": {
-        "50": "#eceff1",
-        "100": "#cfd8dc",
-        "200": "#b0bec5",
-        "300": "#90a4ae",
-        "400": "#78909c",
-        "500": "#607d8b",
-        "600": "#546e7a",
-        "700": "#455a64",
-        "800": "#37474f",
-        "900": "#263238"
-    }
-}
-
+import colorsJSON from "./colors.json";
 /**
  *
  *
@@ -317,9 +19,13 @@ class ColorsHelper {
      * @param {string} [subCode=""] - A subcode to the main color which darkens it as it is increased.
      * @param {boolean} [hex=true] - false: returns a number instead of color with "#" prefix
      * @returns {string} || {number} || false
+     * @author Tayfun Turgut <tyfn.trgt@gmail.com>
+     * @author Tunahan Gormus <tunahangormus@gmail.com>
+     * 
      * @memberof ColorsHelper
      */
     static getColor(colorString = "", subCode = "", hex = true) {
+        let colorsWithSubs = false;
         // validate parameters
         if ((typeof colorString) == "string") {
             if (colorString.length == 0) {
@@ -327,23 +33,47 @@ class ColorsHelper {
                 Log => colorString: ${colorString}`);
                 return false;
             } else {
-                if (!colors[colorString]) {
-                    console.warn(`ColorsHelper.GetColor : Invalid color string!
-                    Log => colorString: ${colorString}`);
-                    return false;
+                if (subCode.length == 0) {
+                    if (!colorsJSON.colorPalette[colorString]) {
+                        if (!colorsJSON.colorsWithSubs[colorString]) {
+                            console.warn(`ColorsHelper.GetColor : Invalid color string!
+                            Log => colorString: ${colorString}`);
+                            return false;
+                        } else {
+                            colorsWithSubs = true;
+                        }
+                    }
                 } else {
-                    if (!(typeof subCode) == "string") {
-                        console.warn(`ColorsHelper.GetColor : Sub code must be of type "integer"!
-                        Log => subCode: ${subCode}`);
-                        return false;
+
+                    if (!colorsJSON.colorsWithSubs[colorString]) {
+                        if (!colorsJSON.colorPalette[colorString]) {
+                            console.warn(`ColorsHelper.GetColor : Invalid color string!
+                            Log => colorString: ${colorString}`);
+                            return false;
+                        } else {
+                            colorsWithSubs = false;
+                        }
                     } else {
-                        if (!colors[colorString][subCode]) {
-                            console.warn(`ColorsHelper.GetColor : Invalid sub code
+
+                        if (!(typeof subCode) == "string") {
+                            console.warn(`ColorsHelper.GetColor : Sub code must be of type "integer"!
                             Log => subCode: ${subCode}`);
                             return false;
+                        } else {
+
+                            if (!colorsJSON.colorsWithSubs[colorString][subCode]) {
+                                console.warn(`ColorsHelper.GetColor : Invalid sub code
+                                Log => subCode: ${subCode}`);
+                                return false;
+                            } else {
+                                colorsWithSubs = true;
+                            }
                         }
                     }
                 }
+
+
+
             }
         } else {
             console.warn(`ColorsHelper.GetColor : Color string must be of type "string"!
@@ -351,13 +81,140 @@ class ColorsHelper {
             return false;
         }
 
-        if (hex) {
-            return colors[colorString][String(subCode)];
+        if (colorsWithSubs) {
+            if (subCode.length == 0) {
+                subCode = 900;
+            }
+
+            if (hex) {
+                return colorsJSON.colorsWithSubs[colorString][String(subCode)];
+            } else {
+                // convert color to hexadecimal number
+                return Phaser.Display.Color.HexStringToColor(colorsJSON.colorsWithSubs[colorString][String(subCode)]).color;
+            }
         } else {
-            // convert color to hexadecimal number
-            return Phaser.Display.Color.HexStringToColor(colors[colorString][String(subCode)]).color;
+            if (hex) {
+                return colorsJSON.colorPalette[colorString];
+            } else {
+                // convert color to hexadecimal number
+                return Phaser.Display.Color.HexStringToColor(colorsJSON.colorPalette[colorString]).color;
+            }
         }
+
+
     }
+
+    /**
+     * @description Returns a random color
+     *
+     * @static
+     * @memberof ColorsHelper
+     * @param {boolean} [hex=true] - false: returns a number instead of color with "#" prefix
+     * 
+     * @author Tunahan Gormus <tunahangormus@gmail.com>
+     * @returns {string}
+     */
+    static getRandomColor(hex = true) {
+        let randomColor = (Math.random() * 0xFFFFFF << 0).toString(16);
+        return hex ? "#" + randomColor : "0x" + randomColor;
+    }
+
+    /**
+     * @description Returns a gradient color array from given start color and end color.
+     *
+     * @static
+     * @memberof ColorsHelper
+     * @param {string} [startColor = ""] A string representing "start color" of the gradient array.
+     * @param {string} [endColor = ""] A string representing "end color" of the gradient array.
+     * @param {number} [colorCount = 10] Size of the gradient array.
+     * @param {boolean} [hex=true] - false: returns a number instead of color with "#" prefix
+     * 
+     * @author Tunahan Gormus <tunahangormus@gmail.com>
+     * @author Euler Junior <https://stackoverflow.com/users/5274306/euler-junior>
+     * @returns {array}
+     * 
+     */
+    static gradientColor(startColor = "", endColor = "", colorCount = 10, hex = true) {
+
+        let start = this.convertToRGB(startColor);
+        let end = this.convertToRGB(endColor);
+
+        let alpha = 0.0;
+
+        let colors = [];
+
+        for (var i = 0; i < colorCount; i++) {
+            let c = [];
+            alpha += (1.0 / colorCount);
+
+            c[0] = start[0] * alpha + (1 - alpha) * end[0];
+            c[1] = start[1] * alpha + (1 - alpha) * end[1];
+            c[2] = start[2] * alpha + (1 - alpha) * end[2];
+
+            colors.push(this.convertToHex(c, hex));
+
+        }
+
+        return colors;
+    }
+
+    /**
+     * @description Convert Rgb to hex string
+     *
+     * @static
+     * @memberof ColorsHelper
+     * @param {array} [rgb] An array which contains the RGB values
+     * @param {string} [hex = "true"] false: returns a number instead of color with "#" prefix
+     * 
+     * @author Tunahan Gormus <tunahangormus@gmail.com>
+     * @returns {array}
+     * 
+     */
+    static convertToHex(rgb, hex = "true") {
+        let calculateHex = function (c) {
+            let s = "0123456789abcdef";
+            let i = parseInt(c);
+            if (i == 0 || isNaN(c))
+                return "00";
+            i = Math.round(Math.min(Math.max(0, i), 255));
+            return s.charAt((i - i % 16) / 16) + s.charAt(i % 16);
+        }
+
+        let hexColor = calculateHex(rgb[0]) + calculateHex(rgb[1]) + calculateHex(rgb[2]);
+        return hex ? "#" + hexColor : "0x" + hexColor;
+    }
+
+
+    /**
+     * @description Convert a hex string to an RGB triplet
+     *
+     * @static
+     * @memberof ColorsHelper
+     * @param {string} [hex = ""] Hex string "can start with '#' or '0x' or without any"
+     * 
+     * @author Tunahan Gormus <tunahangormus@gmail.com>
+     * @returns {array}
+     */
+    static convertToRGB(hex) {
+        /* Removes '#' and '0x' in color hex string */
+
+        let trim = function (s) {
+            if (s.charAt(0) == '#') {
+                return s.substring(1, 7);
+            } else if (s.charAt(0) == '0' && s.charAt(1) == 'x') {
+                return s.substring(2, 8);
+            } else {
+                return s;
+            }
+        };
+
+        let color = [];
+        color[0] = parseInt((trim(hex)).substring(0, 2), 16);
+        color[1] = parseInt((trim(hex)).substring(2, 4), 16);
+        color[2] = parseInt((trim(hex)).substring(4, 6), 16);
+        return color;
+    }
+
 
     /**
      * @description Lists the properties of the helper to the console
